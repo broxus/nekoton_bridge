91019a022376b9c4c52bce6adc44c3879e037f54
nesquikm <drapolyuk@gmail.com>
2023-03-17 17:06:05 +0400

    fix: remove reflectable files

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,6 +4,8 @@
 find . -type f -name "*_generated.*" -delete
 find . -type f -name "*.freezed.dart" -delete
 find . -type f -name "*.g.dart" -delete
+find . -type f -name "*.reflectable.dart" -delete
+
 rm -rdf packages/nekoton_bridge/native/src/merged.rs
 rm -rdf packages/flutter_nekoton_bridge/ios/Classes/frb.h
 
