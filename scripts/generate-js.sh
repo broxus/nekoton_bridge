b9bf994cbddb3fa2bf61004fa3bd920e83a07939
nesquikm <drapolyuk@gmail.com>
2023-04-18 16:20:01 +0400

    feat: add JS bundle check

--- scripts/generate-js.sh
+++ scripts/generate-js.sh
@@ -5,3 +5,13 @@ npm ci
 npm run build
 npm run clean
 
+if [[ $1 == "test" ]]; then
+    if git diff --exit-code; then
+        echo "No changes, all ok"
+        exit 0
+    else
+        echo "Changes detected, please commit them before running tests"
+        exit 1
+    fi
+fi
+
