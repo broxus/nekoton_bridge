6bf2cf59ce86c86a8ef567d93ed117cddc1beef2
nesquikm <drapolyuk@gmail.com>
2023-04-21 17:15:24 +0400

    fix: generated code test workflow

--- scripts/git-check.sh
+++ scripts/git-check.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-if [[ $1 == "test" ]]; then
-    if git diff --exit-code; then
-        echo "No changes, all ok"
-        exit 0
-    else
-        echo "Changes detected, please commit them before running tests"
-        exit 1
-    fi
+if git diff --exit-code; then
+    echo "No changes, all ok"
+    exit 0
+else
+    echo "Changes detected, please commit them before running tests"
+    exit 1
 fi
