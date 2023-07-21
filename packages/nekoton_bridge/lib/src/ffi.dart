694cae99342888c996d9e896dbc2f93d8c641e90
Alex Adrianov <32712725+Alex-A4@users.noreply.github.com>
2023-07-21 18:11:47 +0300

    feat: allow mocking bridge (#172)

--- packages/nekoton_bridge/lib/src/ffi.dart
+++ packages/nekoton_bridge/lib/src/ffi.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 import 'bridge_generated.dart';
 export 'bridge_generated.dart';
 import 'ffi/stub.dart'
@@ -5,11 +7,23 @@ import 'ffi/stub.dart'
     if (dart.library.html) 'ffi/web.dart';
 import 'package:nekoton_bridge/nekoton_bridge.dart';
 
+/// Support function that allows create library not in [createLib] but
+/// in [createWrapper] allowing us to mock bridge wrapper via [mockWrapper].
+typedef ExternalLibraryCreator = ExternalLibrary Function();
+
 NekotonBridge? _wrapper;
 
-NekotonBridge createWrapper(ExternalLibrary lib) {
-  _wrapper ??= createWrapperImpl(lib);
+NekotonBridge createWrapper(ExternalLibraryCreator creator) {
+  _wrapper ??= createWrapperImpl(creator());
   return _wrapper!;
 }
 
-NekotonBridge createLib() => createWrapper(createLibraryImpl());
+/// Mock bridge library for test.
+/// To setup wrapper, put [mock] instance to allow [createLib] to avoid creating
+/// native library.
+@visibleForTesting
+void mockWrapper(NekotonBridge mock) {
+  _wrapper = mock;
+}
+
+NekotonBridge createLib() => createWrapper(createLibraryImpl);
