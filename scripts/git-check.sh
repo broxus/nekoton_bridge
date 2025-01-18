a58fa67275feede86c482add04ac9d679735c0c3
Egor Komarov <egor.komarov@bf.rocks>
2025-01-18 11:34:57 +0100

    ci: ignore lock files changes

--- scripts/git-check.sh
+++ scripts/git-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if git diff --exit-code ':!packages/nekoton_bridge/lib/src/bridge_generated*.dart'; then
+if git diff --exit-code ':!packages/nekoton_bridge/lib/src/bridge_generated*.dart' ':!*.lock'; then
     echo "No changes, all ok"
     exit 0
 else
